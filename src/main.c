39926ddd9912292585f3720769311e3a83642b44
Chris Pickel <sfiera@twotaled.com>
2026-06-25 11:31:08 +0900

    Fix off-by-one on rotated level

--- src/main.c
+++ src/main.c
@@ -77,14 +77,14 @@ IWRAM_DATA struct {
 } bg2;
 
 IWRAM_CODE void rotate(u8 angle) {
-    s16 cos   = sin_table[(angle + 64) & 0xFF];
-    s16 sin   = sin_table[angle];
-    bg2.pa = 2 * cos;
-    bg2.pb = 2 * sin;
-    bg2.pc = 2 * -sin;
-    bg2.pd = 2 * cos;
-    bg2.x  = 168 * 0x100 - (cos * 2 * 120 + sin * 2 * 80);
-    bg2.y  = 168 * 0x100 - (-sin * 2 * 120 + cos * 2 * 80);
+    s16 cos = sin_table[(angle + 64) & 0xFF];
+    s16 sin = sin_table[angle];
+    bg2.pa  = 2 * cos;
+    bg2.pb  = 2 * sin;
+    bg2.pc  = 2 * -sin;
+    bg2.pd  = 2 * cos;
+    bg2.x   = 168 * 0x100 - (cos * (2 * 120 - 1) + sin * (2 * 80 - 1));
+    bg2.y   = 168 * 0x100 - (-sin * (2 * 120 - 1) + cos * (2 * 80 - 1));
 
     for (u8 i = 0; i < sprite_count; ++i) {
         s8 x = sprite_locs[i].x, y = sprite_locs[i].y;
@@ -111,10 +111,10 @@ void set_tile(u8 x, u8 y, u8 value) {
 
 void set_orb(u8 x, u8 y, u8 value) {
     set_tile(x, y, 2);
-    u8 idx             = sprite_count++;
-    sprite_locs[idx].x = 78 - x * 12;
-    sprite_locs[idx].y = 78 - y * 12;
-    shadow.sprites[idx].attr2     = (value * 4) | ATTR2_PRIORITY(0) | ATTR2_PALETTE(0);
+    u8 idx                    = sprite_count++;
+    sprite_locs[idx].x        = 78 - x * 12;
+    sprite_locs[idx].y        = 78 - y * 12;
+    shadow.sprites[idx].attr2 = (value * 4) | ATTR2_PRIORITY(0) | ATTR2_PALETTE(0);
 }
 
 IWRAM_CODE int main() {
@@ -134,7 +134,7 @@ IWRAM_CODE int main() {
     REG_BG2CNT = BG_SIZE_2 | BG_256_COLOR | CHAR_BASE(0) | SCREEN_BASE(8);
 
     for (size_t i = 0; i < 128; ++i) {
-        OAM[i].attr0 = 191;
+        OAM[i].attr0            = 191;
         shadow.sprites[i].attr0 = 191;
     }
     for (size_t i = 0; i < orbsPalLen / 2; ++i) {
@@ -195,8 +195,8 @@ IWRAM_CODE int main() {
         REG_BG2PB = bg2.pb;
         REG_BG2PC = bg2.pc;
         REG_BG2PD = bg2.pd;
-        REG_BG2X = bg2.x;
-        REG_BG2Y = bg2.y;
+        REG_BG2X  = bg2.x;
+        REG_BG2Y  = bg2.y;
         DMA3COPY(&shadow, OAM, 128 | DMA16 | DMA_IMMEDIATE);
     }
 }
