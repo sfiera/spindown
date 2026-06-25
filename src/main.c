494b94dd045d811f83c270b316cf2881a68afaef
Chris Pickel <sfiera@twotaled.com>
2026-06-25 10:57:26 +0900

    Copy sprites from shadow OAM

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <gba_dma.h>
 #include <gba_input.h>
 #include <gba_interrupt.h>
 #include <gba_sprites.h>
@@ -35,12 +36,12 @@ static level_t level = {
     "....######....",
 };
 
-void interrupt() {
+IWRAM_CODE void interrupt() {
     REG_IF = IRQ_VBLANK;
     REG_IFBIOS |= IRQ_VBLANK;
 }
 
-s16 sin_table[256] = {
+IWRAM_DATA s16 sin_table[256] = {
     0,    6,    12,   18,   25,   31,   37,   43,   49,   56,   62,   68,   74,   80,   86,   92,
     97,   103,  109,  115,  120,  126,  131,  136,  142,  147,  152,  157,  162,  167,  171,  176,
     181,  185,  189,  193,  197,  201,  205,  209,  212,  216,  219,  222,  225,  228,  231,  234,
@@ -62,25 +63,33 @@ s16 sin_table[256] = {
 typedef struct {
     s8 x, y;
 } sprite_loc_t;
-sprite_loc_t sprite_locs[128] = {
-};
+IWRAM_DATA sprite_loc_t sprite_locs[128];
+IWRAM_DATA u8           sprite_count = 0;
 
-u8 sprite_count = 0;
+IWRAM_DATA union {
+    OBJATTR   sprites[128];
+    OBJAFFINE affine[32];
+} shadow;
 
-void rotate(u8 angle) {
-    s16 cos    = sin_table[(angle + 64) & 0xFF];
-    s16 sin    = sin_table[angle];
-    REG_BG2PA = 2 * cos;
-    REG_BG2PB = 2 * sin;
-    REG_BG2PC = 2 * -sin;
-    REG_BG2PD = 2 * cos;
-    REG_BG2X  = 168 * 0x100 - (cos * 2 * 120 + sin * 2 * 80);
-    REG_BG2Y  = 168 * 0x100 - (-sin * 2 * 120 + cos * 2 * 80);
+IWRAM_DATA struct {
+    s16 pa, pb, pc, pd;
+    s32 x, y;
+} bg2;
+
+IWRAM_CODE void rotate(u8 angle) {
+    s16 cos   = sin_table[(angle + 64) & 0xFF];
+    s16 sin   = sin_table[angle];
+    bg2.pa = 2 * cos;
+    bg2.pb = 2 * sin;
+    bg2.pc = 2 * -sin;
+    bg2.pd = 2 * cos;
+    bg2.x  = 168 * 0x100 - (cos * 2 * 120 + sin * 2 * 80);
+    bg2.y  = 168 * 0x100 - (-sin * 2 * 120 + cos * 2 * 80);
 
     for (u8 i = 0; i < sprite_count; ++i) {
         s8 x = sprite_locs[i].x, y = sprite_locs[i].y;
-        OAM[i].attr0 = (74 - ((cos * y + sin * x) >> 8));
-        OAM[i].attr1 = (114 - ((-sin * y + cos * x) >> 8)) | OBJ_SIZE(1);
+        shadow.sprites[i].attr0 = (74 - ((cos * y + sin * x) >> 8));
+        shadow.sprites[i].attr1 = (114 - ((-sin * y + cos * x) >> 8)) | OBJ_SIZE(1);
     }
 }
 
@@ -102,13 +111,13 @@ void set_tile(u8 x, u8 y, u8 value) {
 
 void set_orb(u8 x, u8 y, u8 value) {
     set_tile(x, y, 2);
-    u8 idx = sprite_count++;
+    u8 idx             = sprite_count++;
     sprite_locs[idx].x = 78 - x * 12;
     sprite_locs[idx].y = 78 - y * 12;
-    OAM[idx].attr2 = (value * 4) | ATTR2_PRIORITY(0) | ATTR2_PALETTE(0);
+    shadow.sprites[idx].attr2     = (value * 4) | ATTR2_PRIORITY(0) | ATTR2_PALETTE(0);
 }
 
-int main() {
+IWRAM_CODE int main() {
     REG_DISPCNT = LCDC_OFF;  // Enable forced blank
 
     u32* tileset = CHAR_BASE_ADR(0);
@@ -124,8 +133,9 @@ int main() {
     }
     REG_BG2CNT = BG_SIZE_2 | BG_256_COLOR | CHAR_BASE(0) | SCREEN_BASE(8);
 
-    for (size_t i = 4; i < 128; ++i) {
+    for (size_t i = 0; i < 128; ++i) {
         OAM[i].attr0 = 191;
+        shadow.sprites[i].attr0 = 191;
     }
     for (size_t i = 0; i < orbsPalLen / 2; ++i) {
         OBJ_COLORS[i] = orbsPal[i];
@@ -159,11 +169,6 @@ int main() {
     u16 last_keys = REG_KEYINPUT;
     s16 turning   = 0;
     while (true) {
-        // VBlankIntrWait();
-        while (REG_VCOUNT < 160) {
-        }
-        while (REG_VCOUNT == 160) {
-        }
         if (turning) {
             angle += turning;
             rotate(angle >> 8);
@@ -179,5 +184,19 @@ int main() {
             }
             last_keys = REG_KEYINPUT;
         }
+
+        // VBlankIntrWait();
+        while (REG_VCOUNT < 160) {
+        }
+        while (REG_VCOUNT == 160) {
+        }
+
+        REG_BG2PA = bg2.pa;
+        REG_BG2PB = bg2.pb;
+        REG_BG2PC = bg2.pc;
+        REG_BG2PD = bg2.pd;
+        REG_BG2X = bg2.x;
+        REG_BG2Y = bg2.y;
+        DMA3COPY(&shadow, OAM, 128 | DMA16 | DMA_IMMEDIATE);
     }
 }
